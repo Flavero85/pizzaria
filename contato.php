14991c342a9bda5f9a9cc3785d64d3809fc9d208
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2024-04-02 19:05:13 -0300

    up

---
+++ contato.php
@@ -0,0 +1,31 @@
+<?php include ("cabecalho.php")?>
+<?php include ("menu.php")?>
+
+<main class="bg-light row pe-3 ps-3">
+            <div class="row">
+                <h2 class="mt-4 h1 ">Entre em contato</h2>
+                <hr><br>
+                
+                    <div class="row col-4">
+                    
+                        <input class="form-control" type="text" placeholder="Nome" aria-label="nome">
+                        <input class="form-control" type="text" placeholder="Email" aria-label="Email">
+                        <input class="form-control" type="text" value="Assunto" aria-label="Assunto">
+                        <label for="Mensagem" class="form-label"></label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <input type="button" value="Enviar">
+
+                   
+                       
+                    </div>
+                    
+                    
+                    <div class = "col-6">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14716.750786358292!2d-47.320706478710925!3d-22.758414617660844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8998174c896b9%3A0x739b7f99b3323574!2sSenac%20Americana!5e0!3m2!1spt-BR!2sbr!4v1712020248863!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>     
+            
+                        </div>
+                    
+                    </div>
+</main>
+
+<?php include("rodape.php") ?>
